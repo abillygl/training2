5b04b9f51555ee8e7ff19abbb5284ffa2ccd554b
abillygl <andrew.billy@goldenliving.com>
2014-02-13 13:20:17 -0600

    Initial

---
+++ training2/Controllers/LettersController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace training2.Controllers
+{
+    public class LettersController : ApiController
+    {
+        private 
+
+    }
+}
